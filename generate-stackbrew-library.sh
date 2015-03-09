2a937fa3f4e75dd6307f63518764a8ca48f801f7
James <james@resin.io>
2015-03-09 17:54:43 +0000

    Dockerfiles added

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -22,45 +22,52 @@ for repo in "${repos[@]}"; do
 	cd $repo
 	versions=( */ )
 	versions=( "${versions[@]%/}" )
-	cd ..
-	url='git://github.com/resin-io-library/docker-node'
-	for version in "${versions[@]}"; do
-		commit="$(git log -1 --format='format:%H' -- "$repo/$version")"
-		fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$repo/$version/Dockerfile" | cut -d' ' -f3)"
-		versionAliases=( $fullVersion $version ${aliases[$fullVersion]} )
-
-		echo
-		for va in "${versionAliases[@]}"; do
-			echo "$va: ${url}@${commit} $repo/$version"
-		done
 	
-		for variant in onbuild slim wheezy; do
-			commit="$(git log -1 --format='format:%H' -- "$repo/$version/$variant")"
+	url='git://github.com/nghiant2710/docker-node'
+	for version in "${versions[@]}"; do
+		cd $version
+		fullVersions=( */ )
+		fullVersions=( "${fullVersions[@]%/}" )
+		cd ..
+		for fullVersion in "${fullVersions[@]}"; do
+
+			commit="$(git log -1 --format='format:%H' -- "$repo/$version/$fullVersion")"
+			#fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$repo/$version/Dockerfile" | cut -d' ' -f3)"
+			versionAliases=( $fullVersion $version ${aliases[$fullVersion]} )
+
 			echo
 			for va in "${versionAliases[@]}"; do
-				if [ "$va" = 'latest' ]; then
-					va="$variant"
-				else
-					va="$va-$variant"
-				fi
-				echo "$va: ${url}@${commit} $repo/$version/$variant"
+				echo "$va: ${url}@${commit} $repo/$version/$fullVersion"
 			done
+		
+			for variant in onbuild slim wheezy; do
+				commit="$(git log -1 --format='format:%H' -- "$repo/$version/$fullVersion/$variant")"
+				echo
+				for va in "${versionAliases[@]}"; do
+					if [ "$va" = 'latest' ]; then
+						va="$variant"
+					else
+						va="$va-$variant"
+					fi
+					echo "$va: ${url}@${commit} $repo/$version/$fullVersion/$variant"
+				done
+			done
+
+			# Only for armv7hf
+			if [ $repo == 'armv7hf' ]; then
+				variant='sid'
+				commit="$(git log -1 --format='format:%H' -- "$repo/$version/$fullVersion/$variant")"
+				echo
+				for va in "${versionAliases[@]}"; do
+					if [ "$va" = 'latest' ]; then
+						va="$variant"
+					else
+						va="$va-$variant"
+					fi
+					echo "$va: ${url}@${commit} $repo/$version/$fullVersion/$variant"
+				done
+			fi			
 		done
-
-		# Only for armv7hf
-		if [ $repo == 'armv7hf' ]; then
-			variant='sid'
-			commit="$(git log -1 --format='format:%H' -- "$repo/$version/$variant")"
-			echo
-			for va in "${versionAliases[@]}"; do
-				if [ "$va" = 'latest' ]; then
-					va="$variant"
-				else
-					va="$va-$variant"
-				fi
-				echo "$va: ${url}@${commit} $repo/$version/$variant"
-			done
-		fi
-
 	done
+	cd ..
 done
